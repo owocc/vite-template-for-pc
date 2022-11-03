bbe7abff70ce2e411880dfe47e85feaddf5b38ae
小吉君 <owocc@qq.com>
2022-11-03 16:42:58 +0800

    add

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-if="$route.query.meta.is"></div>
+</template>
+<script setup>
+import router from '@/router';
+const route = router.currentRoute.value 
+console.log(route)
+</script>
+<style lang='scss' scoped>
+
+</style>
